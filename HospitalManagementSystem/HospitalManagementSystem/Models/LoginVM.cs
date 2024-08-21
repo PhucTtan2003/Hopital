91d0478bf499977583df9d1e7ec8be0a3b7e77a1
King Fat <120268913+ASTRAUNATFF@users.noreply.github.com>
2024-08-21 14:13:44 +0700

    File Tổng

---
+++ HospitalManagementSystem/HospitalManagementSystem/Models/LoginVM.cs
@@ -0,0 +1,11 @@
+﻿namespace HospitalManagementSystem.Models
+{
+    public class LoginVM
+    {
+        public string Username { get; set; } = null!;
+
+        public string PasswordAccount { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+    }
+}
