3de943fa4ba0917df0a1e0bd68de1f6acd4c4b4c
Nguyễn Tấn Nhã <99543691+NguyenTanNHa@users.noreply.github.com>
2024-08-28 15:17:33 +0700

    Phân Quyền User và Admin - Trang Giao Diện của Admin

---
+++ Phân Quyền User và Admin - Trang Giao diện Admin/HospitalManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagementSystem.Controllers
+{
+    [Authorize(Roles = "admin")]  // Chỉ cho phép người dùng với vai trò 'admin' truy cập
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ Phân Quyền User và Admin - Trang Giao diện Admin/HospitalManagementSystem/Data/Appointment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalManagementSystem.Data;
+
+public partial class Appointment
+{
+    public int AppointmentId { get; set; }
+
+    public int PatientId { get; set; }
+
+    public int DoctorId { get; set; }
+
+    public DateOnly AppointmentDate { get; set; }
+
+    public TimeOnly AppointmentTime { get; set; }
+
+    public string? Notes { get; set; }
+
+    public string? Username { get; set; }  // Thêm thuộc tính Username
+
+
+    public virtual Doctor Doctor { get; set; } = null!;
+
+    public virtual Patient Patient { get; set; } = null!;
+}
