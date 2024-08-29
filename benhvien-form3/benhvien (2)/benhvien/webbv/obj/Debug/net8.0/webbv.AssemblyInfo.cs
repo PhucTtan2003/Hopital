f4099df0a308014e70ff1e9cc813db7595d20800
nguyenvanthinh992003 <141216726+nguyenvanthinh992003@users.noreply.github.com>
2024-08-29 14:20:22 +0700

    form trang chủ

--- benhvien-form3/benhvien (2)/benhvien/webbv/obj/Debug/net8.0/webbv.AssemblyInfo.cs
+++ benhvien-form3/benhvien (2)/benhvien/webbv/obj/Debug/net8.0/webbv.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webbv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f5a3cac0495e10bba48d6e407bc6a003ec63ae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("webbv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webbv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
